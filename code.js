1bfe2e9680532934790acc6871c43956a7918cfa
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-02-29 12:22:52 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+    let visited = new Set();
+    function dfs(currentNode) {
+        if (currentNode === targetNode) {
+            return [currentNode];
+        }
+        visited.add(currentNode);
+        for (let neighbor of graph[currentNode]) {
+            if (!visited.has(neighbor)) {
+                let path = dfs(neighbor);
+                if (path.length > 0) {
+                    return [currentNode, ...path];
+                }
+            }
+        }
+        return [];
+    }
+    return dfs(startNode);
 }
+
+module.exports = depthFirstSearch;
