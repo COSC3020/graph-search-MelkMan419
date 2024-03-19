fca71c8449fc5933b658b442c85aa1bcc380ef12
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-03-19 12:33:33 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,20 +1,31 @@
-function depthFirstSearch(graph, startNode, targetNode) {
-    if (startNode === targetNode) return [startNode];
-    // For this algorithm, we will assume an adjacency matrix with a value of 1
-    // representing a path exists and 2 representing that it exists and has been visited
-    var pos = null;
-    var nodeList = [];
-
-    for (var i = 0; i < graph.length; i++) {
-        pos = graph[startNode][i];
-        if (pos === 1) {
-            if (i === targetNode) return [i];
-            graph[startNode][i] = 2;
-            nodeList = nodeList.concat(i, depthFirstSearch(graph, i, targetNode));
+function search(graph, startNode, targetNode, visitedNodes, path) {
+      if (startNode === targetNode) {
+          visitedNodes.push(startNode);
+          path.push(startNode);
+          return true;
+      }
+    visitedNodes.push(startNode);
+    path.push(startNode);
+    for (let i = 0; i < graph[startNode].length; i++) {
+        let node = graph[startNode][i];
+        if (visitedNodes.indexOf(node) === -1) {
+            if (search(graph, node, targetNode, visitedNodes, path)) {
+                return true;
+            }
         }
-        if (nodeList[nodeList.length - 1] != targetNode) {nodeList = []; }
     }
+    path.pop();
+    return false;
+}
 
-    return nodeList;
+function depthFirstSearch(graph, startNode, targetNode){
+    let visitedNodes = [];
+    let path = [];
+    if (graph.length === 0 || search(graph, startNode, targetNode, visitedNodes, path) === false){
+        return [];
+    }
+    else {
+        return path;
+    }
 }
 module.exports = depthFirstSearch;
